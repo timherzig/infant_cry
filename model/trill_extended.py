590e3f4cf2784d33fc691af994f23850db0f9b5f
Tim Herzig <tim.herzig@hotmail.com>
2023-06-08 15:20:30 +0200

    razie conv layers

--- model/trill_extended.py
+++ model/trill_extended.py
@@ -11,6 +11,15 @@ def trill(config):
     embeddings = m(input)["embedding"]
     x = layers.Dropout(config.dropout)(embeddings)  # embeddings
 
+    # Razieh Impl.
+    x = layers.Reshape((-1, 128, 1))(x)  # Reshape the input to a 2D array
+    x = layers.Conv2D(
+        filters=32, kernel_size=(3, 3), padding="same", activation="relu"
+    )(x)
+    x = layers.MaxPooling2D(pool_size=(2, 2))(
+        x
+    )  # Add MaxPooling layer after the Conv2D
+
     if config.bi_lstm.use:
         x = tf.expand_dims(x, axis=1)
         x = layers.Bidirectional(layers.LSTM(config.bi_lstm.units))(x)
@@ -25,16 +34,6 @@ def trill(config):
     #     filters=1, kernel_size=(1, 1), padding="valid", activation="linear"
     # )(x)
     # x = layers.Flatten()(x)
-
-    # Razieh Impl.
-    x = layers.Reshape((-1, 128, 1))(x)  # Reshape the input to a 2D array
-    x = layers.Conv2D(
-        filters=32, kernel_size=(3, 3), padding="same", activation="relu"
-    )(x)
-    x = layers.MaxPooling2D(pool_size=(2, 2))(
-        x
-    )  # Add MaxPooling layer after the Conv2D
-    x = layers.Flatten()(x)
 
     x = layers.Dense(config.dense, activation="relu")(x)
     predictions = layers.Dense(config.output, activation="sigmoid")(x)
