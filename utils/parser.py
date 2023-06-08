38b09158852c7d2a47c35a6d56655f94c780f659
Tim Herzig <tim.herzig@hotmail.com>
2023-06-08 15:39:03 +0200

    changes conv layer dimensions and added pooling

--- utils/parser.py
+++ utils/parser.py
@@ -5,7 +5,7 @@ def parse_arguments():
     parser = ArgumentParser()
 
     parser.add_argument(
-        "--config", type=str, default="config/config.yaml", help="Path to config file"
+        "--config", type=str, default="/netscratch/herzig/shared_projects/infant_cry/config/trill1_noaug.yaml", help="Path to config file"
     )
     parser.add_argument(
         "--checkpoint", type=str, default=None, help="Path to checkpoint file"
